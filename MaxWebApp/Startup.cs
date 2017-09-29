ae751543e37db76fb32d2f64456ad9bef8ba7bfe
Majid Mahboobipour <m.mahboobi@gmail.com>
2017-09-29 23:16:26 +1000

    Initial Commit

---
+++ MaxWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaxWebApp.Startup))]
+namespace MaxWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
