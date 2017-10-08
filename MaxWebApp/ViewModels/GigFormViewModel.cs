f39d3b76c6c17b60558a3ae6009b710225a3343e
Majid Mahboobipour <m.mahboobi@gmail.com>
2017-10-08 10:49:17 +1000

    Create Add a Gig Form

---
+++ MaxWebApp/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MaxWebApp.Models;
+using System.Collections.Generic;
+
+namespace MaxWebApp.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres;
+    }
+}
